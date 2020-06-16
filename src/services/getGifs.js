dddc1aa4174ee13b73855ef4ce85290e40940287
CGUTIERREZ <carlos5t@hotmail.com>
2020-06-16 06:36:09 -0500

    Comentando Servicio

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,6 +1,6 @@
 const apiKey = 'XLJpuabV4LYDFiyXixAnLkMnBmZFjlIr'
 
-
+//Consumiiendo API
 export default function getGifs({keyword = 'morty'} = {}) {
     const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=G&lang=en`
     return fetch(apiUrl)
